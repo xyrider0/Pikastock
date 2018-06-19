a6a2609b5bb1dbaa549cf427b0d5c313f7d1a7a1
Veerr0o <https://veerr0o@bitbucket.org/Vp0p/p0p_main.git>
2018-06-19 17:04:51 -0400

    Init Commit

---
+++ src/main/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+     $("#sample").click(function(){
+        alert("i clicked dis");
+    });
+    //bootstrap JS
+    $('#bootSample').on('click', function(event) {
+      alert("I am Fabulous");
+    });
+});
